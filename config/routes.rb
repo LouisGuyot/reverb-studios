79ae7d84f39da3fd547da9f10221926db5ba3fcd
louis frederic schefer <lf.schefer@gmail.com>
2022-11-21 15:18:13 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "studios#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :studios, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
 end

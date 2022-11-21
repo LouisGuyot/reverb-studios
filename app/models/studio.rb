d2570839a3efcf89842bad270ed06db0797f3b58
louis frederic schefer <lf.schefer@gmail.com>
2022-11-21 15:33:13 +0100

    valadation models

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,3 +1,7 @@
 class Studio < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, uniqueness: true
+  validates :address, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
 end

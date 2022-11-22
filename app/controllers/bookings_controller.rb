d95ee4a178c4148ce937207fcd13400c889d5ce8
Marie Santi <marie.santi@penninghen.fr>
2022-11-22 11:40:17 +0100

    avancement

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+  def new
+    @studio = Studio.find(params[:studio_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @studio = Studio.find(params[:studio_id])
+    @booking = Booking.new(booking_params)
+    @booking.studio = @studio
+    @booking.save
+    redirect_to booking_path(@booking)
+  end
+
+   private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :studio)
+  end
+
+
 end

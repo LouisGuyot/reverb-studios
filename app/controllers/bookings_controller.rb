701da2d5c758ee9989b7f0967d47d9c1debf84ff
Andris Asper <andris.asper@gmail.com>
2022-11-23 14:20:21 +0100

    booking delete works

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,8 @@
 class BookingsController < ApplicationController
+  # skip_before_action :authenticate_user!, only: %i[index show create new destroy]
+  before_action :set_booking, only: %i[destroy show]
+  before_action :set_studio, only: %i[destroy]
+
   def new
     @studio = Studio.find(params[:studio_id])
     @booking = Booking.new
@@ -21,11 +25,28 @@ class BookingsController < ApplicationController
   end
 
   def show
-    @booking = Booking.find(params[:id])
     authorize @booking
   end
 
+  def destroy
+    if current_user.id == @booking.user_id
+      @booking.destroy
+      redirect_to studio_bookings_path status: :see_other
+      authorize @booking
+    else
+      return
+    end
+  end
+
   private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_studio
+    @studio = Studio.find(params[:studio_id])
+  end
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date, :studio_id, :user_id)
